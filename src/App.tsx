a81848c7f5599493c155f075af57d92eb3540429
Dariusz Krukowski <dariusz.krukowski@knowit.eu>
2022-06-27 20:46:58 +0200

    Try to get data from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  async function getData() {
+    try {
+      const response = await axios.get('http://webtabsyapi.execa.pl/medicine');
+      return response
+    } catch (error) {
+      alert(error);  
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+          { getData() }
         </p>
         <a
           className="App-link"
