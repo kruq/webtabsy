ea892a28dfd1d29236480bf63d79e373b92ec352
Dariusz Krukowski <dariusz.krukowski@knowit.eu>
2022-07-04 04:25:50 +0200

    Use default Azure API url

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,10 @@ interface IMedicine {
 
 function App() {
 
-  const apiUrl = 'https://webtabsyapi.execa.pl/medicine'
+//  const apiUrl = 'https://webtabsyapi.execa.pl/medicine'
 //  const apiUrl = 'https://localhost:7078/medicine'
+  const apiUrl = 'https://wwebtabsyapi.azurewebsites.net/medicine'
+
 
   const [medicines, setMedicines] = useState<IMedicine[]>([]);
   const [newMedicineName, setNewMedicineName] = useState('');
