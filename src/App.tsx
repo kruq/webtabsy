c5f83154c26172f24c9818a41f198b138c3145ae
kruq <dkrukowski@windowslive.com>
2022-07-11 14:27:58 +0300

    Display date in correct format

--- src/App.tsx
+++ src/App.tsx
@@ -120,7 +120,7 @@ function App() {
       </header>
       <section>
         <div>
-          <p>Ostatnio oznaczone jako wzięte <strong>{ medicines?.length > 0 && medicines[0]?.lastDateTaken?.toString() }</strong></p>
+          <p>Ostatnio oznaczone jako wzięte <strong>{ medicines?.length > 0 && new Date(medicines[0]?.lastDateTaken?.toString()).toLocalDateString() }</strong></p>
           <p><button onClick={handleTakeMedicines}>Weź leki</button></p>
         </div>
         <hr />
